6a24a863a651c86c2120b2073b370355ae2b8af9
Mustafa badr <36304069+moustafaBadrr@users.noreply.github.com>
2019-12-26 18:16:26 +0200

    Add files via upload

---
+++ Bonus JQuery.js
@@ -0,0 +1,34 @@
+
+$(function(){  /// To the button sign up and log in and skip
+    $("button").mouseenter(function(){
+        $(this).css("color", "blue");
+    });
+});
+$(function(){ 
+    $("button").mouseleave(function(){
+        $(this).css("color", "black");
+    });
+});
+
+$(function(){  // Just To Show the JQuery Effects...
+    $("h1").fadeIn(6000);
+})
+
+function gotosignup() {
+    $(function () {
+        window.location.replace("Sign-up.html");
+    }, 0)
+}
+
+function gotologin() {
+    $(function () {
+        window.location.replace("Login.html");
+    }, 0)
+}
+
+function gotoHome() {
+
+    $(function () {
+        window.location.replace("Home/Home.html");
+    }, 0)
+}
