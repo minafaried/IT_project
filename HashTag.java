2a2dc26277752c606b76389622a886715c7e3ac5
Mustafa badr <36304069+moustafaBadrr@users.noreply.github.com>
2019-12-28 17:53:07 +0200

    Add files via upload

---
+++ HashTag.java
@@ -0,0 +1,21 @@
+
+public class HashTag {
+	String name;
+	int numOfPosts;
+	public String getName() {
+		return name;
+	}
+	public int getNumOfPosts() {
+		return numOfPosts;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setNumOfPosts(int numOfPosts) {
+		this.numOfPosts = numOfPosts;
+	}
+	
+	public void addHasTag() {
+		
+	}
+}
