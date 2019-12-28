6a18711eeeba6c20f5e49bdd0dae29919c806f2d
mina faried <36136028+minafaried@users.noreply.github.com>
2019-12-28 14:16:24 +0200

    Update SignUp.php

--- SignUp.php
+++ SignUp.php
@@ -3,28 +3,22 @@ $user_name = $_POST["username"];
 $email = $_POST["email"];
 $password = $_POST["password"];
 $phone = $_POST["phone"];
-$admin_code = $_POST["adminCode"]
+$admin_code = $_POST["adminCode"];
 
-mysqli("localhost","root","","it");
+$connectioni=new mysqli("localhost","root","","it");
+$q = "INSERT INTO user(`username`,`email`,`password`,`phone`,`adminCode`) 
+VALUES ('$user_name','$email','$password','$phone','$admin_code')";
 
-$q="INSERT INTO user(`username`,`email`,`password`,`phone`,`adminCode`) VALUES ('$user_name','$email','$password','$phone','$admin_code')";
-
-
-if ($q) {
+if ($connectioni->query($q) === TRUE) {
     session_start();
     $_SESSION['username'] = $user_name;
     echo "<script>
     alert('Registertion is completed');
     window.location.href='Home.html';
     </script>";
- 
-
-}
-else{ 
+} else {
     echo "<script>
     alert('Registertion is failed, this username is existed');
     window.location.href='Sign-up.html';
     </script>";
 }
-mysql_close();
-?>
