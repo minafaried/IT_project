beea3ae3faa30dda06928c29285af7eff2818eae
Mustafa badr <36304069+moustafaBadrr@users.noreply.github.com>
2019-12-28 17:57:11 +0200

    Add files via upload

---
+++ Message.java
@@ -0,0 +1,30 @@
+
+public class Message {
+	String from;
+	String to;
+	String content;
+
+	public String getFrom() {
+		return from;
+	}
+
+	public String getTo() {
+		return to;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setFrom(String from) {
+		this.from = from;
+	}
+
+	public void setTo(String to) {
+		this.to = to;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+}
